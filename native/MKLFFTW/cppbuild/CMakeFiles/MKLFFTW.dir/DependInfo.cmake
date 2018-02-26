bbb0aa257365f464f2a4e5c1d1b34e0ec8608a40
Brian Northan <bnorthan@gmail.com>
2018-02-26 06:31:56 -0800

    WIP Linux build

---
+++ native/MKLFFTW/cppbuild/CMakeFiles/MKLFFTW.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/bnorthan/code/imagej/ops-experiments/native/MKLFFTW/src/MKLFFTW.cpp" "/home/bnorthan/code/imagej/ops-experiments/native/MKLFFTW/cppbuild/CMakeFiles/MKLFFTW.dir/src/MKLFFTW.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/opt/intel/mkl/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ native/MKLFFTW/cppbuild/CMakeFiles/MKLFFTW.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MKLFFTW.dir/src/MKLFFTW.o"
+  "libMKLFFTW.pdb"
+  "libMKLFFTW.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MKLFFTW.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
